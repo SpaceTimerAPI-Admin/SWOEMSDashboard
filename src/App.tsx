25b8702130684acfd37fbf9774af7244282f6ff4
SpaceTimerAPI-Admin <Anthony.mchughnh@gmail.com>
2026-02-26 14:59:43 -0500

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import RequireAuth from "./components/RequireAuth";
-import Home from "./pages/Home";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import Login from "./pages/Login";
 import Enroll from "./pages/Enroll";
+import Home from "./pages/Home";
 import Tickets from "./pages/Tickets";
 import TicketNew from "./pages/TicketNew";
 import TicketDetail from "./pages/TicketDetail";
@@ -11,10 +10,22 @@ import Projects from "./pages/Projects";
 import ProjectNew from "./pages/ProjectNew";
 import ProjectDetail from "./pages/ProjectDetail";
 import EOD from "./pages/EOD";
+import Settings from "./pages/Settings";
+import BottomNav from "./components/BottomNav";
+import { isAuthed } from "./lib/auth";
+
+function RequireAuth({ children }: { children: React.ReactNode }) {
+  const loc = useLocation();
+  if (!isAuthed()) return <Navigate to="/login" replace state={{ from: loc.pathname }} />;
+  return <>{children}</>;
+}
 
 export default function App() {
+  const loc = useLocation();
+  const showNav = !["/login", "/enroll"].includes(loc.pathname);
+
   return (
-    <BrowserRouter>
+    <>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/enroll" element={<Enroll />} />
@@ -30,7 +41,12 @@ export default function App() {
         <Route path="/projects/:id" element={<RequireAuth><ProjectDetail /></RequireAuth>} />
 
         <Route path="/eod" element={<RequireAuth><EOD /></RequireAuth>} />
+        <Route path="/settings" element={<RequireAuth><Settings /></RequireAuth>} />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
+
+      {showNav ? <BottomNav /> : null}
+    </>
   );
 }
