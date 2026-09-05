c61e236b5b07fa16972e531454bdac5145cdca5e
SpaceTimerAPI-Admin <Anthony.mchughnh@gmail.com>
2026-09-05 09:17:14 -0400

    Add files via upload

--- src/pages/XmasTickets.tsx
+++ src/pages/XmasTickets.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { getToken } from "../lib/auth";
 
 async function apiFetch(path: string, opts: RequestInit = {}) {
   const res = await fetch(path, {
